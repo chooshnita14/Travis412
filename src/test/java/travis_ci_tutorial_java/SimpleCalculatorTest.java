c6119e0582e5cf50e9e4721c38692daa76dfcfad
chooshnita <chooshnita@localhost>
2026-03-01 15:21:09 -0500

    Added multiply and divide methods - TDD completed

--- src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
+++ src/test/java/travis_ci_tutorial_java/SimpleCalculatorTest.java
@@ -1,15 +1,37 @@
 package travis_ci_tutorial_java;
 
-import java.util.*;
-
 import static org.junit.Assert.*;
-import org.junit.*;
+import org.junit.Test;
 
 public class SimpleCalculatorTest {
-	@Test
-	
-	public void testMinus() {
-	    SimpleCalculator calc = new SimpleCalculator();
-	    assertEquals(calc.minus(5, 3), 2);
-	}
+
+    @Test
+    public void testAdd() {
+        SimpleCalculator calc = new SimpleCalculator();
+        assertEquals(2, calc.add(1, 1));
+    }
+
+    @Test
+    public void testMinus() {
+        SimpleCalculator calc = new SimpleCalculator();
+        assertEquals(2, calc.minus(5, 3));
+    }
+
+    @Test
+    public void testMultiply() {
+        SimpleCalculator calc = new SimpleCalculator();
+        assertEquals(12, calc.multiply(4, 3));
+    }
+
+    @Test
+    public void testDivide() {
+        SimpleCalculator calc = new SimpleCalculator();
+        assertEquals(2, calc.divide(6, 3));
+    }
+
+    @Test(expected = ArithmeticException.class)
+    public void testDivideByZero() {
+        SimpleCalculator calc = new SimpleCalculator();
+        calc.divide(5, 0);
+    }
 }
